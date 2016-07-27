9f1aca934be723e4decfd6aa00b691586181eb36
Lawrence <lawrencedawson@hotmail.co.uk>
2016-07-27 19:44:22 +0100

    Dictionary sort complete

--- ch10-nothing-new/dictionary_sort.rb
+++ ch10-nothing-new/dictionary_sort.rb
@@ -1,3 +1,30 @@
-def dictionary_sort arr
-  # your code here
+def dictionary_sort(list)
+  repeat list, []
+end
+  def repeat unsorted, sorted
+      if unsorted.length <=0
+        return sorted
+    end
+    min = unsorted.pop
+    still_unsorted = []
+
+    unsorted.each do |checking|
+    	if checking.downcase < min.downcase
+    		still_unsorted.push min
+    		min = checking
+    	else
+    		still_unsorted.push checking
+    	end
+    end
+
+    sorted.push min
+
+    repeat(still_unsorted, sorted)
+
+  end
+
+  puts dictionary_sort(["z","V","c","a"])
+
+
+
 end
