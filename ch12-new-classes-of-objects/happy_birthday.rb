939c6e171d1056702bcecc59380945dc69c81741
Lawrence <lawrencedawson@hotmail.co.uk>
2016-08-01 14:26:35 +0100

    Completed Happy Birthday! Task

--- ch12-new-classes-of-objects/happy_birthday.rb
+++ ch12-new-classes-of-objects/happy_birthday.rb
@@ -1,1 +1,26 @@
-# your code here
+puts
+puts
+puts 'Hi there, I want to tell you how old you are and SPANK you for every birthday you/ve had?!?'
+puts
+puts 'Please enter the year you were born'
+year = gets.chomp
+puts
+puts 'Please enter the month you were born'
+month = gets.chomp
+puts
+puts 'Please enter the day you were born'
+day = gets.chomp
+
+dob = Time.local(year, month, day)
+seconds_alive = Time.new - dob
+hours = seconds_alive / (60 * 60)
+minutes = (seconds_alive / 60) % 60
+seconds = seconds_alive % 60
+days = hours / 24
+birthdays = days / 365.25
+
+puts 'You were born on ' + dob.to_s
+puts
+puts 'You/ve had ' + birthdays.to_s + 'so here/s that many SPANKS!'
+puts
+puts 'SPANK! ' * birthdays
--- ch12-new-classes-of-objects/one_billion_seconds.rb
+++ ch12-new-classes-of-objects/one_billion_seconds.rb
@@ -1,1 +1,27 @@
-# your code here
+time_now = Time.now
+
+my_birth = Time.local(1987,8,2,18,00)
+
+how_long_ago = time_now - my_birth
+seconds_to_bill = 1_000_000_000 - how_long_ago
+
+hours = seconds_to_bill / (60 * 60)
+minutes = (seconds_to_bill / 60) % 60
+seconds = seconds_to_bill % 60
+days = hours / 24
+actual_day = (Time.local(1987,8,2,18,00) + 10**9)
+
+puts
+puts 'I was born ' +  my_birth.to_s
+puts
+puts 'That was ' + how_long_ago.to_s + ' seconds ago'
+puts
+puts 'I/ll be a billion seconds old in ' + seconds_to_bill.to_s + ' seconds'
+puts 'which is ' + days.to_s + ' days'
+puts hours.to_s + ' hours'
+puts minutes.to_s + ' minutes'
+puts 'and ' + seconds.to_s + ' seconds from now'
+puts
+puts 'On ' + actual_day.to_s + 'to be precise'
+puts
+puts
